60eb550e850bf48e58b5791f919798e83c8f6c89
STSonyThomas <goodtobetrue2@gmail.com>
2021-08-23 09:23:19 +0530

    The final change

--- LMS.c
+++ LMS.c
@@ -23,27 +23,75 @@ int main(){
     int i,j,k,count=0,num=0;
     i=j=count;
     k=num;
+    //Heading of LMS
+    printf("\n\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**");
+    printf("\n\t\t        ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~");
+    printf("\n\t\t        ~                 WELCOME                   ~");
+    printf("\n\t\t        ~                   TO                      ~");
+    printf("\n\t\t        ~                 LIBRARY                   ~");
+    printf("\n\t\t        ~               MANAGEMENT                  ~");
+    printf("\n\t\t        ~                 SYSTEM                    ~");
+    printf("\n\t\t        ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~");
+    printf("\n\t\t  **-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**-**\n");
     //add admin portal
+    char username[]="meclibrary";
+    char password[]="librmec";
+    int flag=0;
+    printf("Enter your username\n");
+    scanf("%s",username);
+    if(strcmp(username,"meclibrary")==0)
+    {
+        printf("Enter the password\n");
+        scanf("%s",password);
+        if(strcmp(password,"librmec")==0)
+        {
+            printf("Password correct...\n");
+            printf("Login successful...\n");
+            printf("\n");
+            printf("\n");
+            
+        }
+        else
+        {
+            printf("Incorrect password\n");
+            printf("Try again\n");
+            flag++;
+        }
+    }
+    else
+    {
+        printf("Incorrect username\n");
+        printf("Try again\n");
+        flag++;
+    }
+    //end of admin portal
+    //checkpoint
+    if(flag!=0){
+        printf("You have entered wrong username or password contact admin for assistance\n");
+        exit(0);
+    }
+
     while(j!=8){
         printf("To add new books enter 1\n");
         printf("To get all books details enter 2\n");
         printf("To get books by specific author enter 3\n");
         printf("To get details of specific book  enter 4\n");
         printf("To get total number of books enter 5\n");
-        printf("To checkout a book enter 6");
-        printf("To add a new student enter 7");
+        printf("To checkout a book enter 6\n");
+        printf("To add a new student enter 7\n");
         printf("To exit enter 8\n");
+        printf("Enter your response = ");
         scanf("%d",&j);
         switch(j){
             //to add book
             case 1:
             printf("Enter book name = ");
             scanf("%s",l[i].bookName);
-            printf("ENter author name");
+            printf("ENter author name = ");
             scanf("%s",l[i].author);
-            printf("Enter number of pages");
+            printf("Enter number of pages = ");
             scanf("%d",&l[i].pages);
-            printf("Enter price");
+            printf("Enter price = ");
             scanf("%f",&l[i].price);
             count++;
             break;
@@ -51,7 +99,9 @@ int main(){
             case 2:
             for(i=0;i<count;i++){
                 printf("%s\t %s\t %d\t %f\t",l[i].author,l[i].bookName,l[i].pages,l[i].price);
+                printf("\n");
                 printf("****************************************");
+                printf("\n");
 
             }
             if(count==0){
@@ -120,64 +170,6 @@ int main(){
         }
         
     }
-    /*IVIDETHOTT THAZHE ULLATH ONNUM NOOKANDA NJAN UNIQUE CODE VECH ENTHENKILUM CHEYYAN PATUMO ENN NOIYATHA :)
-    int studentNumber;
-    char action,studentName[studentNumber];
-    int database[studentNumber][4];
-    printf("Enter number of Young Readers today");
-    scanf("%d",&studentNumber);
-    for(int i=0;i<studentNumber;i++){
-        printf("Enter name of student no %d = ",i+1);
-        scanf("%s",&studentName[i]);
-        printf("\n");
-        database[i][1]=100+i;
-        printf("Unique code for student generated! = %d",database[i][1]);
-        printf("\n");
-        printf("Data entered. Thank You!");
-        printf("\n");
-        system("clear");
-    }
-    //for adding new student and other functions
-    printf("Would you like to exit(e),CheckOut(o)");
-        scanf("%c",&action);
-    do{
-        if (action=='o'){
-            int readyCheck;
-            printf("Enter number of Readers ready for checkout");
-            scanf("%d",readyCheck);
-            //segmentation error
-            for(int j=1;j<=readyCheck;j++){
-                char name[20];
-                int index;
-                printf("Enter name of checkoutee");
-                scanf("%s",&name);
-                //to find the index of the checkoutee
-                for(int k=0;k<studentNumber;k++){
-                    if(studentName[k]==name){
-                        index=k;
-
-                    }
-                }
-                //the code prompts for checking out
-                printf("Enter name code of book taken = ");
-                scanf("%d",&database[index][2]);
-                printf("\n)");
-                printf("Enter date of issue = ");
-                scanf("%d",&database[index][3]);
-                printf("\n)");
-                printf("Enter date of return = ");
-                scanf("%d",&database[index][4]);
-                printf("\n)");
-            }
-        
-        
-        }
-        else{
-            printf("exiting....");
-        }
-        printf("Would you like to exit(e),CheckOut(o)");
-        scanf("%c",&action);
-    }while(action!='e');*/
     
     return 0;
 }
